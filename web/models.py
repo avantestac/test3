11fbb81c5afd02c308cd832e3ea526314108f5a4
av <av@localhost.localdomain>
2023-07-05 19:37:18 +0330

    2 commit

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+    
