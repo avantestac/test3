4c56bee80aae2c7bcf2324ec8792500eb92f9ef8
av <av@localhost.localdomain>
2023-07-05 20:14:05 +0330

    Expense model is there

--- web/models.py
+++ web/models.py
@@ -7,5 +7,5 @@ class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     
